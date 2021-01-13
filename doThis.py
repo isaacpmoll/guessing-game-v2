df7e4d641a0a7f109b3e5c49f18ba447c190d1c0
isaacpmoll <73903177+isaacpmoll@users.noreply.github.com>
2021-01-13 12:09:28 -0500

    Update doThis.py

--- doThis.py
+++ doThis.py
@@ -1,3 +1,24 @@
 #imports:
 
+import time
+import random
+
 #code:
+pick = -1101010101010101
+name = input("What is your name? ")
+print("Well then, " +name+ ", let's play a guessing game!")
+time.sleep(1)
+mi = int(input("Pick the minimum range: "))
+ma = int(input("Pick the maximum range: "))
+ran = random.randint(mi, ma)
+
+while pick != ran:
+	pick = int(input("Pick a number within the range of " + str(mi) + " and " + str(ma) + ":"))
+	if pick > ran:
+		print("\nToo high!")
+		time.sleep(0.3)
+	elif pick < ran:
+		print("\nToo low.")
+		time.sleep(0.3)
+	else:
+		print("You got it!")
